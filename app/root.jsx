951e3b35433111647297ff2de586986658a72ff3
rojesh <rojace2011@gmail.com>
2023-09-18 19:03:38 +0545

    Error handling via ErrorBoundary.

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,7 @@
 import MainNavigation from '~/components/MainNavigation';
 import styles from '~/styles/main.css';
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -28,6 +29,34 @@ export default function App() {
           <MainNavigation />
         </header>
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({error}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>An error occured!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occured!</h1>
+          {/* <p>{error.message}</p> */}
+          <p>{error}</p>
+          <p>Back to <Link to="/">Safety! </Link></p>
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
 import NewNote, {links as newNoteLinks } from '~/components/NewNote';
 import NoteList, {links as noteListLinks} from '~/Components/NoteList';
 import { getStoredNotes, storeNotes } from '../data/notes';
@@ -40,4 +40,16 @@ export async function action({request}) {
 
 export function links() {
   return [...newNoteLinks(), ...noteListLinks()]
+}
+
+
+export function ErrorBoundary({error}) {
+  return (
+    <main className="error">
+      <h1>An error related to your notes occured!</h1>
+      {/* <p>{error.message}</p> */}
+      <p>{error}</p>
+      <p>Back to <Link to="/">Safety! </Link></p>
+    </main>
+  )
 }
