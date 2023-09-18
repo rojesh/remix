1a4a898a9b90164b51f9ce8906e3327f617add4b
rojesh <rojace2011@gmail.com>
2023-09-18 18:33:47 +0545

    using usenavigation for providing userfeedback.

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,7 +1,10 @@
+import { Form, useNavigation } from '@remix-run/react';
 import styles from './NewNote.css';
-import { Form } from '@remix-run/react';
+
 
 export default function NewNote() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting"
   return (
     <Form method="post" id="note-form">
       <p>
@@ -13,7 +16,9 @@ export default function NewNote() {
         <textarea id="content" name="content" rows="5" required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        <button disabled= {isSubmitting} >
+          {isSubmitting ? "Adding" : "Add Note" }
+        </button> 
       </div>
     </Form>
   );
