d769e1cf076da1bf0a286ea21554e861fc36e583
rojesh <rojace2011@gmail.com>
2023-09-18 18:07:06 +0545

    adding backendcode and redirecting.

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from '@remix-run/node';
 import NewNote, {links as newNoteLinks } from '~/components/NewNote';
+import { getStoredNotes, storeNotes } from '../data/notes';
 
 export default function NotesPage() {
   return (
@@ -8,6 +10,20 @@ export default function NotesPage() {
   )
 }
 
-export function links(){
+export async function action({request}) {
+  const formData = await request.formData();
+  // const noteData = {
+  //   title: formData.get("title"),
+  //   content: formData.get("content")
+  // }
+  const noteData = Object.fromEntries(formData);
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect('/notes');
+}
+
+export function links() {
   return [...newNoteLinks()]
 }
