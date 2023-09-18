3ecd900ec4be8c0fee110cab98ca4fc1775d5142
rojesh <rojace2011@gmail.com>
2023-09-18 17:44:39 +0545

    Add navigation.

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-
+import MainNavigation from '~/components/MainNavigation';
+import styles from '~/styles/main.css';
 import {
   Links,
   LiveReload,
@@ -9,7 +9,6 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import styles from '~/styles/main.css';
 
 // export const links = () => [
 //   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -25,6 +24,9 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
